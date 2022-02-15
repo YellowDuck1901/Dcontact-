c46c4d083099b8bd97dc7f6d8a4b851d85289199
phu <phunvce150611@fpt.edu.vn>
2022-02-15 07:30:00 +0700

    js giờ nằm trong thư mục MyScript

--- Dcontact/Controllers/AccountController.cs
+++ Dcontact/Controllers/AccountController.cs
@@ -6,25 +6,12 @@ using System.Web.Mvc;
 
 namespace Dcontact.Controllers
 {
-    public class HomeController : Controller
+    public class AccountController : Controller
     {
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
