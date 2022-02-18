415527a96ae2fc6f08d027664096fb8d8dc09a0c
phu <phunvce150611@fpt.edu.vn>
2022-02-18 11:05:42 +0700

    Account-Home

---
+++ Dcontact/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Dcontact
+{
+    public class Class1
+    {
+        public String username { get; set; }
+        public String email { get; set; }
+        public String password { get; set; }
+        public string password2 { get; set; }
+
+
+    }
+}
