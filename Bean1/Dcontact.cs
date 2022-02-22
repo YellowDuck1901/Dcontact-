a4378f80eb06a7c4c6a7fb1411c4619d9915e8f4
AOY311 <cathuan113@gmail.com>
2022-02-22 14:35:12 +0700

    add obj Dcontact and Row

---
+++ Bean1/Dcontact.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bean
+{
+    public class Dcontact
+    {
+        public string numerView;
+        public string avt;
+        List<Row> rows;
+
+        public Dcontact(string numerView, string avt, List<Row> rows)
+        {
+            this.numerView = numerView;
+            this.avt = avt;
+            this.rows = rows;
+        }
+
+        public Dcontact()
+        {
+        }
+    }
+}
